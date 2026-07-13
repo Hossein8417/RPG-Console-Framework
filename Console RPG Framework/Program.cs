c1d5a8925676508d103b69bd0b1e7cb57eb9b7e9
Hossein8417 <hosseinnardini19@gmail.com>
2026-07-13 14:43:45 +0330

    basic Error handling (using try and catch) added to the app

--- Console RPG Framework/Program.cs
+++ Console RPG Framework/Program.cs
@@ -1,24 +1,21 @@
 ﻿using System;
+using static System.Net.Mime.MediaTypeNames;
 
 class Program {
     public static bool AppLoop = true;
     public static void Main(string[] args)
     {
-        while (AppLoop)
+
+        try
         {
-            AppFlow.SectionSwitcher();
+            while (AppLoop)
+            {
+                AppFlow.SectionSwitcher();
+            }
+        }
+        catch (Exception e)
+        {
+            throw e;
         }
     }
 }
-
-
-//for next commit
-
-//try
-//{
-    
-//}
-//catch (Exception e)
-//{
-//    throw e;
-//}
